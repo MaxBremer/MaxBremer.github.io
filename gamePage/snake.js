f025df15fc5597d0b3d269742d15b478eb6c9ce2
MaxBremer <wmaxonbremer@gmail.com>
2020-06-27 19:55:04 -0500

    little features

--- gamePage/snake.js
+++ gamePage/snake.js
@@ -3,10 +3,12 @@ window.onload = function(){
 		ctx = canv.getContext("2d");
 		document.addEventListener("keydown", keyPush);
 		setInterval(game, 1000/15);
+		highscore = 0;
 		setup();
 	}
 	function setup(){
 		stupidMode = false;
+		score = 0;
 		headCol = document.getElementById("snakeColor").value;
 		tailCol = "purple";
 		appleCol = document.getElementById("appleColor").value;
@@ -22,17 +24,32 @@ window.onload = function(){
 		tailLen = 3;
 		started = false;
 		grad = ctx.createRadialGradient(canv.width/2, canv.height/2, 40, canv.width/2, canv.height/2, 250);
-		grad.addColorStop(0, "rgb(153, 230, 0)");
-		grad.addColorStop(1, "rgb(117, 178, 72)");
+		bgcol1 = document.getElementById("bgCol1").value;
+		bgcol2 = document.getElementById("bgCol2").value;
+		grad.addColorStop(0, bgcol1);
+		grad.addColorStop(1, bgcol2);
 		ctx.fillStyle = grad;
 		ctx.fillRect(0, 0, canv.width, canv.height);
 		writeSquare(appleCol, ax, ay);
 		writeSquare(headCol, px, py);
+		ctx.fillStyle = "black";
+		ctx.font = "18px Sans-Serif";
+		ctx.fillText("Score: " + score, canv.width - 100, 36);
 	}
 
 	function refresh(){
+		grad = ctx.createRadialGradient(canv.width/2, canv.height/2, 40, canv.width/2, canv.height/2, 250);
+		bgcol1 = document.getElementById("bgCol1").value;
+		bgcol2 = document.getElementById("bgCol2").value;
+		grad.addColorStop(0, bgcol1);
+		grad.addColorStop(1, bgcol2);
+		ctx.fillStyle = grad;
+		ctx.fillRect(0, 0, canv.width, canv.height);
 		headCol = document.getElementById("snakeColor").value;
 		appleCol = document.getElementById("appleColor").value;
+		ctx.fillStyle = "black";
+		ctx.font = "18px Sans-Serif";
+		ctx.fillText("Score: " + score, canv.width - 100, 36);
 		writeSquare(appleCol, ax, ay);
 		for(var i = 0; i < tail.length; i++){
 			writeSquare(headCol, tail[i].x, tail[i].y);
@@ -44,16 +61,20 @@ window.onload = function(){
 	
 	function game() {
 		canPush = true;
-		headCol = document.getElementById("snakeColor").value;
-		appleCol = document.getElementById("appleColor").value;
-		//writeSquare(headCol, px, py);
 		if(play && started){
 			grad = ctx.createRadialGradient(canv.width/2, canv.height/2, 40, canv.width/2, canv.height/2, 250);
-			grad.addColorStop(0, "rgb(153, 230, 0)");
-			grad.addColorStop(1, "rgb(117, 178, 72)");
+			grad.addColorStop(0, document.getElementById("bgCol1").value);
+			grad.addColorStop(1, document.getElementById("bgCol2").value);
 			ctx.fillStyle = grad;
 			ctx.fillRect(0, 0, canv.width, canv.height);
+			ctx.fillStyle = "black";
+			ctx.font = "18px Sans-Serif";
+			ctx.fillText("Score: " + score, canv.width - 100, 36);
+			headCol = document.getElementById("snakeColor").value;
+			appleCol = document.getElementById("appleColor").value;
 			writeSquare(appleCol, ax, ay);
+			writeSquare(headCol, px, py);
+			//writeSquare(appleCol, ax, ay);
 			px += xvel;
 			py += yvel;
 			if(px < 0 || px > tileCount || py < 0 || py > tileCount){
@@ -62,6 +83,7 @@ window.onload = function(){
 			if(px == ax && py == ay){
 				ax = Math.floor(Math.random() * tileCount);
 				ay = Math.floor(Math.random() * tileCount);
+				score += 10;
 				tailLen += 1;
 			}
 			while(tail.length > tailLen){
@@ -86,9 +108,17 @@ window.onload = function(){
 	function lose(){
 		started = false;
 		ctx.fillStyle = "red";
-		ctx.font = "36px Sans-Serif";
-		ctx.fillText("You Lose!", 10, 36);
-		ctx.fillText("Press r to restart.", 10, 76);
+		ctx.font = "28px Sans-Serif";
+		if(score > highscore){
+			ctx.fillText("New high score! ", 10, 116);
+			ctx.fillText("" + score + " points!", 10, 156);
+			ctx.fillText("Good job!", 10, 196);
+			highscore = score
+		}else{
+			ctx.fillText("Your high score is:", 10, 116);
+			ctx.fillText("" + highscore + " points", 10, 156);
+		}
+		ctx.fillText("Press r to restart.", 10, 236);
 	}
 	function writeSquare(col, x, y){
 		ctx.fillStyle = col;
